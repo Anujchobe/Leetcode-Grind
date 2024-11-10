3c1b6418ce726cde021df47f0fa817261dec21ca
Anuj K Chobe <98049418+Anujchobe@users.noreply.github.com>
2024-11-10 12:42:13 +0530

    Create 88.java

---
+++ 88.java
@@ -0,0 +1,31 @@
+\\88. Merge Sorted Array
+
+  class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int lst=m+n-1;
+        int a1=m-1;
+        int a2=n-1;
+
+        while(a1>=0 && a2>=0)
+        {
+            if (nums1[a1]>nums2[a2])
+            {
+                nums1[lst]=nums1[a1];
+                a1--;                
+            }
+            else
+            {
+                nums1[lst]=nums2[a2];
+                a2--;
+            }
+            lst--;
+        }
+
+        while(a2>=0)
+        {
+            nums1[lst]=nums2[a2];
+            a2--;
+            lst--;
+        }
+    }
+}
