9d197e82dc8a7407845ebe0282a108f23ed8b4ae
Anuj K Chobe <98049418+Anujchobe@users.noreply.github.com>
2024-11-10 13:30:19 +0530

    Update 27. Remove Element.java

--- 27. Remove Element.java
+++ 27. Remove Element.java
@@ -1,1 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;  // Pointer for the position of elements that are not equal to val
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];  // Copy the non-val element to the k-th position
+                k++;  // Increment k to expand the range of valid elements
+            }
+        }
+        
+        return k;  // k is the count of elements that are not equal to val
+    }
+}
