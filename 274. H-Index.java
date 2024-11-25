e1a50825a4c9cdea8bb159c9a7cd799909be789a
Anuj K Chobe <98049418+Anujchobe@users.noreply.github.com>
2024-11-25 19:56:24 +0530

    Update 274. H-Index.java

--- 274. H-Index.java
+++ 274. H-Index.java
@@ -1,1 +1,16 @@
+Arrays.sort(citations);
+        
+        int n = citations.length;
+        int hIndex = 0;
 
+        // Iterate through the sorted array
+        for (int i = 0; i < n; i++) {
+            // Number of papers with at least citations[i] citations
+            int h = n - i;
+
+            // Check if citations[i] is greater than or equal to h
+            if (citations[i] >= h) {
+                hIndex = h;
+                break;
+            }
+        }
